7f4bc1aa08f9d218b93b1c92892736c8fda378c0
jadie <142028877+ngocmai2518@users.noreply.github.com>
2024-01-07 20:08:43 +0700

    add style and resize function
    allow user input for sizing of the board

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const board = document.querySelector('.board');
-const side = 16;
-for (let i = 0; i <= side - 1; i++) {
-  const row = document.createElement('div');
-  row.classList.add('row')
+let rows = [];
+
+function sizing(side) {
+  // Clear existing rows
+  rows.forEach(row => row.remove());
+  rows = []; // Clear the rows array
   for (let i = 0; i <= side - 1; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    row.appendChild(square);
+    const row = document.createElement('div');
+    row.classList.add('row')
+    for (let j = 0; j <= side - 1; j++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      row.appendChild(square);
+    }
+    board.appendChild(row);
+    rows.push(row); // Add the row to the rows array
   }
-  board.appendChild(row);
 }
+sizing(16);
+
+let sizeInput = document.querySelector('#size-input');
+const note = document.querySelector('#note');
+
+sizeInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      let size = parseInt(sizeInput.value);
+      if (!isNaN(size)) {
+        sizing(size);
+      } else {note.innerHTML = 'Please enter a number';};
+    sizeInput.value = "";
+    }})
+
+        
